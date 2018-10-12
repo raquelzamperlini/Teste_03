6aefbee8dbc5f02e9ea1ca5a407a885d871be422
Aluno <Aluno@L1M15.IPIRANGA.FATEC>
2018-10-11 21:41:45 -0300

    Adoção do Padrão Data Builder para Livro + Suite Class
    - Ajuste dos testes da classe Livro para o padrão Data Builder
    - Criação da classe AllTests Suite para execução de todos os testes

--- sce/src/test/java/com/fatec/sce/UC01CadastrarLivro.java
+++ sce/src/test/java/com/fatec/sce/UC01CadastrarLivro.java
@@ -7,8 +7,139 @@ import org.junit.Test;
 import com.fatec.sce.model.Livro;
 
 public class UC01CadastrarLivro {
+	
+	@Test
+	public void CT01CadastrarLivroComDadosValidos() {
+		try {
+			// Cenário
+			Livro livro;
+			// Ação
+			livro = ObtemLivro.comDadosValidos();
+		} catch (RuntimeException e) {
+			// Verificação
+			fail("Não deve falhar");
+		}
+	}
 
 	@Test
+	public void CT02CadastrarLivroComISBN_em_branco() {
+		// Cenário
+		Livro livro;
+		try {
+			// Ação
+			livro = ObtemLivro.comISBNInvalido_branco();
+			fail("Deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// Verificação
+			assertEquals("ISBN invalido", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void CT03CadastrarLivroComISBN_nulo() {
+		// Cenário
+		Livro livro;
+		try {
+			// Ação
+			livro = ObtemLivro.comISBNInvalido_nulo();
+			fail("Deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// Verificação
+			assertEquals("ISBN invalido", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void CT04CadastrarLivro_com_sucesso_getIsbn() {
+		// Cenário
+		Livro livro = new Livro();
+		livro.setIsbn("121212");
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		
+		assertEquals("121212", livro.getIsbn());
+	}
+	
+	@Test
+	public void CT05CadastrarLivroComTitulo_em_branco() {
+		// Cenário
+		Livro livro;
+		try {
+			// Ação
+			livro = ObtemLivro.comTituloInvalido_branco();
+			fail("Deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// Verificação
+			assertEquals("Titulo invalido", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void CT06CadastrarLivroComTitulo_nulo() {
+		// Cenário
+		Livro livro;
+		try {
+			// Ação
+			livro = ObtemLivro.comTituloInvalido_nulo();
+			fail("Deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// Verificação
+			assertEquals("Titulo invalido", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void CT07CadastrarLivro_com_sucesso_getTitulo() {
+		// Cenário
+		Livro livro = new Livro();
+		livro.setIsbn("121212");
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		
+		assertEquals("Engenharia de Software", livro.getTitulo());
+	}
+	
+	@Test
+	public void CT08CadastrarLivroComAutor_em_branco() {
+		// Cenário
+		Livro livro;
+		try {
+			// Ação
+			livro = ObtemLivro.comAutorInvalido_branco();
+			fail("Deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// Verificação
+			assertEquals("Autor invalido", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void CT09CadastrarLivroComAutor_nulo() {
+		// Cenário
+		Livro livro;
+		try {
+			// Ação
+			livro = ObtemLivro.comAutorInvalido_nulo();
+			fail("Deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// Verificação
+			assertEquals("Autor invalido", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void CT10CadastrarLivro_com_sucesso_getAutor() {
+		// Cenário
+		Livro livro = new Livro();
+		livro.setIsbn("121212");
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		
+		assertEquals("Pressman", livro.getAutor());
+	}
+
+// Código sem refatorar
+/*	@Test
 	public void CT01CadastrarLivroComDadosValidos() {
 		try {
 			// Cenário
@@ -151,5 +282,5 @@ public class UC01CadastrarLivro {
 		
 		assertEquals("Pressman", livro.getAutor());
 	}
-	
+*/	
 }
